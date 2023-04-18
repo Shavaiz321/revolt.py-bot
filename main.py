2e8abe6669e374049c8147e3a8ef5595342c8f9f
Shavaiz_Ahmed <shaviz4444@gmail.com>
2023-04-19 00:59:22 +0500

    Initial Code For Chat Replies

--- main.py
+++ main.py
@@ -3,19 +3,21 @@ import asyncio
 import os
 from dotenv import load_dotenv
 load_dotenv()
-
 TOKEN = os.getenv("TOKEN")
-print(TOKEN)
-
 class Client(revolt.Client):
+    async def get_prefix(self,message: revolt.Message):
+        return "!"
     async def on_message(self, message: revolt.Message):
         message.content= message.content.lower
         if message.content == "hello":
             await message.channel.send("hi how are you")
-
+        elif message.content == "hi":
+            await message.channel.send("Hey there")
+        elif message.content == "oye":
+            await message.reply(f"Han Oye?",True)
 async def main():
     async with revolt.utils.client_session() as session:
-        client = Client(session, f"{TOKEN}")
+        client = Client(session, f"{TOKEN}")#? Your token goes here
         await client.start()
         print("The bot is ready")
 
