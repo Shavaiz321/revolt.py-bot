71dced3bdded268170806434c78e26d8ff2105f6
Shavaiz_Ahmed <shaviz4444@gmail.com>
2023-04-20 01:07:15 +0500

    Working stage with "!" command.

--- main.py
+++ main.py
@@ -3,26 +3,36 @@ import asyncio
 import aiohttp
 import revolt
 from dotenv import load_dotenv
+from revolt.ext import commands
 load_dotenv()
 
 TOKEN = os.getenv("TOKEN")
-class Client(revolt.Client):
-    async def on_message(self, message: revolt.Message):
-            message.content= message.content.lower() #? So every message is lower case
-            if message.content == "hello":
-                await message.channel.send("What do you want?")
-            elif message.content == "hi":
-                await message.channel.send("Hey there")
-            elif message.content == "oye":
-                await message.channel.send("Han Oye?")
-            else:
-                return
-            
-#?This is a simple chat bot, to add more responses you can add on to the existing code
+class Client(commands.CommandsClient):
+    async def get_prefix(self, message: revolt.Message):
+        return "!"
 
+    @commands.command()
+    async def ping(self, ctx: commands.Context):
+        try:
+            await ctx.send("pong")
+            return
+        except Exception as e:
+            print(f"{e} = exception, and the error was caught in the ping block")
+    async def on_message(self, message: revolt.Message):
+        message.content= message.content.lower()
+        await self.process_commands(message)
+        if message.content == "hello":
+            await message.channel.send("hi how are you")
+        elif message.content == "hi":
+            await message.channel.send("Hey there")
+        elif message.content == "oye":
+            await message.channel.send("Han Oye?")
+        else:
+             return
 async def main():
     async with aiohttp.ClientSession() as session:
-        client = Client(session, f"{TOKEN}") #! Input your own token here, I just did this to hide my own token.
+        client = Client(session, f"{TOKEN}")
         print("The bot has started")
         await client.start()
 asyncio.run(main())
+
